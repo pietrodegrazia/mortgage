bdf7cb8d06bde6cf02cb13312f1376f963d1869b
Pietro Degrazia <pietrodegrazia@hotmail.com>
2021-11-06 12:23:12 -0300

    Setup apollo client.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080',
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+    	<App />
+  	</ApolloProvider>,
+	document.getElementById('root')
+);
